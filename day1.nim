208a81ed7b20cacd2eafbaaf00bcf19592145581
lfanew <tyler.bowers@outlook.com>
2025-12-01 23:01:22 -0600

    Consolidated parts. Defined Dial subrange type. Using UFCS w/ mut ref

--- day1.nim
+++ day1.nim
@@ -4,87 +4,43 @@ import utils
 # Shared
 const filename = "input.txt"
 
-const
-  DIAL_LOW = 0
-  DIAL_HIGH = 99
-  OVERFLOW = 100
+type
+  Dial = range[0..99]
 
-block part1:
-  proc left(dial: int, amount: int): int =
-    let amount = amount mod OVERFLOW
-    result = dial - amount
-    if result < DIAL_LOW: result += OVERFLOW
+proc left(dial: var Dial, amount: int): int =
+  result += amount div (Dial.high + 1)
+  let amount = amount mod (Dial.high + 1)
+  for _ in 0 ..< amount:
+    if dial == Dial.low: dial = Dial.high
+    else: dec(dial)
+    if dial == Dial.low: inc(result)
 
-  proc right(dial: int, amount: int): int =
-    let amount = amount mod OVERFLOW
-    result = dial + amount
-    if result > DIAL_HIGH: result -= OVERFLOW
+proc right(dial: var Dial, amount: int): int =
+  result += amount div (Dial.high + 1)
+  let amount = amount mod (Dial.high + 1)
+  for _ in 0 ..< amount:
+    if dial == Dial.high: dial = Dial.low
+    else: inc(dial)
+    if dial == Dial.low: inc(result)
 
-  var dial = 50
-  var answer = 0
+var dial: Dial = 50
+var
+  part1 = 0
+  part2 = 0
 
-  withFile(txt, filename, FileMode.fmRead):
-    var line: string
-    while txt.readLine(line):
-      let op = line[0]
-      let amt: int = (line[1..^1]).parseInt
-      case op:
-        of 'L':
-          dial = left(dial, amt)
-        of 'R':
-          dial = right(dial, amt)
-        else:
-          quit "Invalid operation: " & op
-      if dial == 0: inc(answer)
+withFile(txt, filename, FileMode.fmRead):
+  var line: string
+  while txt.readLine(line):
+    let op = line[0]
+    let amt: int = (line[1..^1]).parseInt
+    case op:
+      of 'L':
+        part2 += dial.left(amt)
+      of 'R':
+        part2 += dial.right(amt)
+      else:
+        quit "Invalid operation: " & op
+    if dial == 0: inc(part1)
 
-  echo answer
-
-# Part 2
-
-block part2:
-  proc left(dial: int, amount: int): (int, int) =
-    var
-      clicks = amount div OVERFLOW
-      position = dial
-    let amount = amount mod OVERFLOW
-
-    for i in 1 .. amount:
-      dec(position)
-      if position < DIAL_LOW: position = DIAL_HIGH
-      if position == DIAL_LOW: inc(clicks)
-
-    return (position, clicks)
-
-  proc right(dial: int, amount: int): (int, int) =
-    var
-      clicks = amount div OVERFLOW
-      position = dial
-    let amount = amount mod OVERFLOW
-
-    for i in 1 .. amount:
-      inc(position)
-      if position > DIAL_HIGH: position = DIAL_LOW
-      if position == DIAL_LOW: inc(clicks)
-
-    return (position, clicks)
-
-  var dial = 50
-  var answer = 0
-
-  withFile(txt, filename, FileMode.fmRead):
-    var line: string
-    while txt.readLine(line):
-      let op = line[0]
-      let amt: int = (line[1..^1]).parseInt
-      case op:
-        of 'L':
-          let (new, clicks) = left(dial, amt)
-          dial = new
-          answer += clicks
-        of 'R':
-          let (new, clicks) = right(dial, amt)
-          dial = new
-          answer += clicks
-        else:
-          quit "Invalid operation: " & op
-  echo answer
+echo "Part 1: ", part1
+echo "Part 2: ", part2

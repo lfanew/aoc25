9d9a39eed5222b0b7aa9e0e4ba7134bce3794569
Tyler Bowers <tbowers@choctawnation.com>
2025-12-02 12:51:55 -0600

    initial day 2 part 2 solution

--- day2.nim
+++ day2.nim
@@ -3,20 +3,58 @@ import utils
 
 const filename = "input.txt"
 
-withFile(f, filename, FileMode.fmRead):
-  var line: string
-  var answer: BiggestInt = 0
-  while f.readLine(line):
-    for idRange in line.split(','):
-      let idRangeParts = idRange.split('-')
-      let idRangeStart = idRangeParts[0].parseBiggestInt
-      let idRangeEnd = idRangeParts[1].parseBiggestInt
-      for id in idRangeStart .. idRangeEnd:
-        var idString = $id
-        if idString.len mod 2 == 1: continue
-        let midpoint = idString.len div 2
-        let left = idString[0 ..< midPoint]       
-        let right = idString[midPoint ..< idString.len]       
-        if left == right: answer += id
+block part1:
+  withFile(f, filename, FileMode.fmRead):
+    var line: string
+    var answer: BiggestInt = 0
+    while f.readLine(line):
+      for idRange in line.split(','):
+        let idRangeParts = idRange.split('-')
+        # TODO: Test performance with mod digit parsing instead of string allocations
+        let idRangeStart = idRangeParts[0].parseBiggestInt
+        let idRangeEnd = idRangeParts[1].parseBiggestInt
+        for id in idRangeStart .. idRangeEnd:
+          var idString = $id
+          if idString.len mod 2 == 1: continue
+          let midpoint = idString.len div 2
+          let left = idString[0 ..< midPoint]       
+          let right = idString[midPoint ..< idString.len]       
+          if left == right: answer += id
 
-  echo answer
+    echo answer
+
+
+block part2:
+  proc isInvalid(id: BiggestInt): bool =
+    var idString = $id
+    let midpoint = idString.len div 2
+    for point in countdown(midpoint, 1):
+      if idString.len mod point != 0:
+        # echo "yikes: ", point
+        continue
+      var parts: seq[string]
+      for i in countup(0, idString.len - 1, point):
+        # echo "i: ", i
+        # echo "point: ", point
+        parts.add(idString[i ..< i + point])
+      # echo idString, ": ", parts
+      var valid = false
+      for i in 1 ..< parts.len:
+        if parts[i] != parts[i-1]: valid = true
+      if not valid: return true
+
+    return false
+    
+  withFile(f, filename, FileMode.fmRead):
+    var line: string
+    var answer: BiggestInt = 0
+    while f.readLine(line):
+      for idRange in line.split(','):
+        let idRangeParts = idRange.split('-')
+        # TODO: Test performance with mod digit parsing instead of string allocations
+        let idRangeStart = idRangeParts[0].parseBiggestInt
+        let idRangeEnd = idRangeParts[1].parseBiggestInt
+        for id in idRangeStart .. idRangeEnd:
+          if id.isInvalid: answer += id
+
+    echo answer

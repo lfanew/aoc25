660e114d3d535719755ce72d6213d3d947e7395b
lfanew <tyler.bowers@outlook.com>
2025-12-01 23:04:00 -0600

    Went back to old loop range, joined variable statements

--- day1.nim
+++ day1.nim
@@ -1,7 +1,6 @@
 import std/strutils
 import utils
 
-# Shared
 const filename = "input.txt"
 
 type
@@ -10,7 +9,7 @@ type
 proc left(dial: var Dial, amount: int): int =
   result += amount div (Dial.high + 1)
   let amount = amount mod (Dial.high + 1)
-  for _ in 0 ..< amount:
+  for _ in 1 .. amount:
     if dial == Dial.low: dial = Dial.high
     else: dec(dial)
     if dial == Dial.low: inc(result)
@@ -18,21 +17,22 @@ proc left(dial: var Dial, amount: int): int =
 proc right(dial: var Dial, amount: int): int =
   result += amount div (Dial.high + 1)
   let amount = amount mod (Dial.high + 1)
-  for _ in 0 ..< amount:
+  for _ in 1 .. amount:
     if dial == Dial.high: dial = Dial.low
     else: inc(dial)
     if dial == Dial.low: inc(result)
 
-var dial: Dial = 50
 var
+  dial: Dial = 50
   part1 = 0
   part2 = 0
 
 withFile(txt, filename, FileMode.fmRead):
   var line: string
   while txt.readLine(line):
-    let op = line[0]
-    let amt: int = (line[1..^1]).parseInt
+    let
+      op = line[0]
+      amt: int = (line[1..^1]).parseInt
     case op:
       of 'L':
         part2 += dial.left(amt)

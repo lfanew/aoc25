7788e6b854e817a6f09bd2eda80ecbbb0661127d
lfanew <tyler.bowers@outlook.com>
2025-12-03 20:43:05 -0600

    init day 3 part 2 solution

--- day3.nim
+++ day3.nim
@@ -1,3 +1,4 @@
+import std/strutils
 import utils
 
 const filename = "input.txt"
@@ -5,25 +6,46 @@ const filename = "input.txt"
 type
   DigitPositions = array[10, seq[int]]
 
-proc highestNumber(positions: DigitPositions): int =
-  for digit1 in countdown(positions.len - 1, 0):
-    if positions[digit1].len == 0: continue
-    let pos1 = positions[digit1][0]
-    for digit2 in countdown(positions.len - 1, 0):
-      if positions[digit2].len == 0: continue
-      let pos2 = positions[digit2][^1]
-      if digit1 == digit2 and pos1 == pos2: continue
-      elif pos2 > pos1: return (digit1 * 10 + digit2)
+block part1:
+  proc highestNumber(positions: DigitPositions): int =
+    for digit1 in countdown(positions.len - 1, 0):
+      if positions[digit1].len == 0: continue
+      let pos1 = positions[digit1][0]
+      for digit2 in countdown(positions.len - 1, 0):
+        if positions[digit2].len == 0: continue
+        let pos2 = positions[digit2][^1]
+        if digit1 == digit2 and pos1 == pos2: continue
+        elif pos2 > pos1: return (digit1 * 10 + digit2)
 
-withFile(f, filename, FileMode.fmRead):
-  var line: string
-  var answer = 0
-  while f.readLine(line):
-    var positions: DigitPositions
-    for idx, ch in line:
-      let digit = ch.parseInt
-      positions[digit].add(idx)
-    answer += highestNumber(positions)
-  echo "Part 1: ", answer
-    # for i, p in positions:
-    #   echo i, ": ", p
+  withFile(f, filename, FileMode.fmRead):
+    var line: string
+    var answer = 0
+    while f.readLine(line):
+      var positions: DigitPositions
+      for idx, ch in line:
+        let digit = ch.parseInt
+        positions[digit].add(idx)
+      answer += highestNumber(positions)
+    echo "Part 1: ", answer
+      # for i, p in positions:
+      #   echo i, ": ", p
+
+block part2:
+  withFile(f, filename, FileMode.fmRead):
+    var line: string
+    var answer = 0
+    while f.readLine(line):
+      var number = ""
+      var remaining = 12
+      var i = 0
+      while number.len < 12:
+        if remaining >= line.len - i:
+          number.add(line[i..^1])
+          break
+        let view = line.toView(i, line.len - remaining)
+        let idx = view.maxIndex
+        number.add view[idx]
+        i += idx + 1
+        dec remaining
+      answer += number.parseInt
+    echo answer
--- utils.nim
+++ utils.nim
@@ -1,3 +1,5 @@
+# Helper templates
+
 template withFile*(f: untyped, filename: string, mode: FileMode, body: untyped) =
   let fn = filename
   var f: File
@@ -9,29 +11,46 @@ template withFile*(f: untyped, filename: string, mode: FileMode, body: untyped) 
   else:
     quit("Cannot open file: " & fn)
 
-# No-copy StringView
-# TODO: Make this a generic view
+# No-copy data view
 
 type
   View*[T] = object
     data: ptr UncheckedArray[T]
     len*: int
 
-proc toView*[T](s: openarray[T], first, last: int): View[T] =
+func toView*[T](s: openarray[T], first, last: int): View[T] =
   assert first >= 0 and last < s.len and first <= last
   result.len = last - first + 1
   result.data = cast[ptr UncheckedArray[T]](addr s[first])
 
-proc `[]`*[T](self: View[T], i: int): T =
+func `[]`*[T](self: View[T], i: int): T =
   assert i >= 0 and i < self.len
   return self.data[i]
 
-proc `==`*[T](self, other: View[T]): bool =
+func `==`*[T](self, other: View[T]): bool =
   if self.len != other.len: return false
   for i in 0 ..< self.len:
     if self[i] != other[i]: return false
 
   return true
+
+func maxIndex*[T](self: View[T]): int =
+  var maxValue = self[0]
+  for i in 1 ..< self.len:
+    let current = self[i]
+    if current > maxValue:
+      maxValue = self[i]
+      result = i
+
+  return result
+
+proc print*[T](self: View[T]) =
+  for i in 0 ..< self.len:
+    stdout.write(self[i])
+    stdout.write(' ')
+  echo ""
+
+# Helper procs
 
 func parseInt*(c: char): int =
   assert c >= '0' and c <= '9'

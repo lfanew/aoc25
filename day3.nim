f670368470bddfb79f9144375c78a96b6c5bcdb8
lfanew <tyler.bowers@outlook.com>
2025-12-03 20:50:46 -0600

    Cleanup and todo

--- day3.nim
+++ day3.nim
@@ -3,10 +3,13 @@ import utils
 
 const filename = "input.txt"
 
-type
-  DigitPositions = array[10, seq[int]]
+# TODO: Better names, address magic constants,
+  # break part 2 into procs, comments to remind myself wth I did
 
 block part1:
+  type
+    DigitPositions = array[10, seq[int]]
+
   proc highestNumber(positions: DigitPositions): int =
     for digit1 in countdown(positions.len - 1, 0):
       if positions[digit1].len == 0: continue
@@ -27,8 +30,6 @@ block part1:
         positions[digit].add(idx)
       answer += highestNumber(positions)
     echo "Part 1: ", answer
-      # for i, p in positions:
-      #   echo i, ": ", p
 
 block part2:
   withFile(f, filename, FileMode.fmRead):
@@ -37,15 +38,15 @@ block part2:
     while f.readLine(line):
       var number = ""
       var remaining = 12
-      var i = 0
+      var idx = 0
       while number.len < 12:
-        if remaining >= line.len - i:
-          number.add(line[i..^1])
+        if remaining >= line.len - idx:
+          number.add(line[idx..^1])
           break
-        let view = line.toView(i, line.len - remaining)
-        let idx = view.maxIndex
-        number.add view[idx]
-        i += idx + 1
+        let view = line.toView(idx, line.len - remaining)
+        let maxIdx = view.maxIndex
+        number.add view[maxIdx]
+        idx += maxIdx + 1
         dec remaining
       answer += number.parseInt
-    echo answer
+    echo "Part 2: ", answer

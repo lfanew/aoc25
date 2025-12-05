807228deb5589c47a98f8ff158157460fec9513a
lfanew <tyler.bowers@outlook.com>
2025-12-05 09:46:30 -0600

    Staged new procs for d5p2

--- day5.nim
+++ day5.nim
@@ -1,7 +1,7 @@
 import std/strutils
 import utils
 
-const filename = "input.txt"
+const filename = "sample.txt"
 
 type
   Range = object
@@ -14,26 +14,56 @@ type
 func contains(range: Range, n: int): bool =
   return n >= range.low and n <= range.high
 
+func overlaps(self, other: Range): bool =
+  return self.contains(other.low) or self.contains(other.high)
+
+func combine(self, other: Range): Range =
+  let low = min(self.low, other.low)
+  let high = max(self.high, other.high)
+  return Range(low: low, high: high)
+
 func parseRange(s: string): Range =
   let parts = s.split('-')
   return Range(low: parts[0].parseInt, high: parts[1].parseInt)
 
-withFile(f, filename, FileMode.fmRead):
-  var answer = 0
-  var pm = ParsingMode.pmRange
-  var line: string
-  var ranges: seq[Range]
-  while f.readLine(line):
-    if line.isEmptyOrWhitespace:
-      pm = ParsingMode.pmId
-      continue
-    case pm:
-      of ParsingMode.pmRange:
-        ranges.add(line.parseRange)
-      of ParsingMode.pmId:
-        let id = line.parseInt
-        for range in ranges:
-          if range.contains(id):
-            inc(answer)
-            break
-  echo answer
+block part1:
+  withFile(f, filename, FileMode.fmRead):
+    var answer = 0
+    var pm = ParsingMode.pmRange
+    var line: string
+    var ranges: seq[Range]
+    while f.readLine(line):
+      if line.isEmptyOrWhitespace:
+        pm = ParsingMode.pmId
+        continue
+      case pm:
+        of ParsingMode.pmRange:
+          ranges.add(line.parseRange)
+        of ParsingMode.pmId:
+          let id = line.parseInt
+          for range in ranges:
+            if range.contains(id):
+              inc(answer)
+              break
+    echo "Part1: ", answer
+
+block part2:
+  withFile(f, filename, FileMode.fmRead):
+    var answer = 0
+    var line: string
+    var ranges: seq[Range]
+    while f.readLine(line):
+      if line.isEmptyOrWhitespace:
+        break
+      ranges.add(line.parseRange)
+    for i in 0 ..< ranges.len:
+      let range1 = ranges[i]
+      for j in i+1 ..< ranges.len:
+        let range2 = ranges[j]
+        if range1.overlaps(range2):
+          echo range1, "|", range2
+          echo "Combining..."
+          let newRange = range1.combine(range2)
+          echo newRange
+        
+    echo "Part1: ", answer

19b834235eae0501f4b8d064dda303b910118d55
lfanew <tyler.bowers@outlook.com>
2025-12-07 17:35:47 -0600

    merged separate dp seq and grid using struct. better cache hits

--- day7.nim
+++ day7.nim
@@ -41,41 +41,46 @@ block part1:
   # grid.print()
   echo answer
 
+type
+  Place = object
+    terrain: char
+    value: int
+
 block part2:
-  var grid: Grid[char]
-  var paths: Grid[int]
+  var grid: Grid[Place]
   withFile(f, filename, FileMode.fmRead):
     var line: string
     while f.readLine(line):
-      let row = cast[seq[char]](line)
+      var row = newSeq[Place](line.len)
+      for i, c in line:
+        if c == 'S': 
+          row[i] = Place(terrain: c, value: 1)
+        else:
+          row[i] = Place(terrain: c, value: 0)
       grid.add(row)
-      let r: Row[int] = newSeq[int](row.len)
-      paths.add(r)
 
-  var start: Point
-  for i, c in grid[0]:
-    if c == 'S':
-      start = (i, 0)
-      break
-
-  paths[start] = 1
-  for point, count in paths:
-    if point[1] == paths.len - 1: break
-    if count == 0: continue
-    let s = grid[point + SOUTH]
-    if s == '^':
+  for point, place in grid:
+    # skip last row
+    if point[1] == grid.len - 1: break
+    # no work to do
+    if place.value == 0: continue
+    let s = point + SOUTH
+    # split?
+    if grid[s].terrain == '^':
       let se = point + SOUTH_EAST
-      if paths.contains(se):
-        paths[se] = paths[se] + count
+      if grid.contains(se):
+        let newPlace = Place(terrain: grid[se].terrain, value: grid[se].value + place.value)
+        grid[se] = newPlace
       let sw = point + SOUTH_WEST
-      if paths.contains(sw):
-        paths[sw] = paths[sw] + count
+      if grid.contains(sw):
+        let newPlace = Place(terrain: grid[sw].terrain, value: grid[sw].value + place.value)
+        grid[sw] = newPlace
+    # flow downwards
     else:
-      paths[point + SOUTH] = paths[point + SOUTH] + count
+      let newPlace = Place(terrain: grid[s].terrain, value: grid[s].value + place.value)
+      grid[s] = newPlace
     
   var answer = 0
-  for path in paths[^1]:
-    answer += path
+  for place in grid[^1]:
+    answer += place.value
   echo answer
-  # grid.print()
-  # paths.print()

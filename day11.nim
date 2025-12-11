25adac9f6acb4d6bb8c4d9b7c82615503da17ec7
Tyler Bowers <tbowers@choctawnation.com>
2025-12-11 12:46:28 -0600

    day 11 part 1 solution

---
+++ day11.nim
@@ -0,0 +1,42 @@
+import std/pegs
+import std/tables
+import utils
+
+const filename = "input.txt"
+
+type
+  Device = ref object
+    id: string
+    outputs: seq[Device]
+
+func `$`(device: Device): string =
+  return device.id
+
+block part1:
+  var nodes: Table[string, Device]
+  withFile(f, filename, FileMode.fmRead):
+    var line: string
+    while f.readLine(line):
+      let parts = line.findAll(peg"\w+")
+      let deviceString = parts[0]
+      if not nodes.contains(deviceString):
+        nodes[deviceString] = Device(id: deviceString, outputs: @[])
+      let device = nodes[deviceString]
+      for outputString in parts[1..^1]:
+        if not nodes.contains(outputString):
+          nodes[outputString] = Device(id: outputString, outputs: @[])
+        let output = nodes[outputString]
+        device.outputs.add(output) 
+
+  let start = nodes["you"]
+  var stack = @[start]
+  var answer = 0
+
+  while stack.len > 0:
+    let current = stack.pop
+    if current.id == "out":
+      inc(answer)
+      continue
+    for output in current.outputs:
+      stack.add(output)
+  echo answer
